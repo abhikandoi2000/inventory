f4e7ad07e9701b99c3c47425fda620d7212ce75f
Abhishek Kandoi <abhikandoi2000@gmail.com>
2013-09-01 19:18:57 +0530

    Support node and python, using g++ for CPP

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-extension='.out'
-outputFile=${1%.*}
-gcc -o $outputFile$extension $1
-./$outputFile$extension
+fullname=$1
+file_ext=${fullname##*.}
+length=$(($#0))
+arguments=${@:2:length}
+if [ $file_ext = "py" ]; then
+  python $fullname $arguments
+elif [ $file_ext = "cpp" ] || [ $file_ext = "cc" ]; then
+  cpp_executable_ext='.out'
+  output_file=${fullname%.*}
+  g++ -o $output_file$cpp_executable_ext $fullname
+  ./$output_file$cpp_executable_ext $arguments
+elif [ $file_ext = "js" ]; then
+  node $fullname $arguments
+fi
