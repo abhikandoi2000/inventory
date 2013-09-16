4c1388d94b731613f560f0c7f89974022af44d7d
Abhishek Kandoi <abhikandoi2000@gmail.com>
2013-09-17 01:52:00 +0530

    Using symlink, colored output.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cp ./run/run /usr/bin
-echo "Reopen your terminal to see the changes"
+
+GREEN=$(tput setaf 2; tput bold)
+NORMAL=$(tput sgr0)
+
+current_dir=`pwd`
+
+# create symlink
+ln -s $current_dir"/run/run" /usr/bin
+
+echo "$GREEN[Done]$NORMAL Reopen your terminal to see the changes"
